048451b4ae10499c1fdca337dee9d46b684f3e97
Matt Toensing <matthew.toensing@gmail.com>
2021-07-19 08:40:16 -0700

    Add test for showing studio movies and include on html page

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -12,14 +12,27 @@ RSpec.describe 'studio index page' do
       expect(page).to have_content("Studio Location: #{studio1.location}")
       expect(page).to have_content("Studio Name: #{studio2.name}")
       expect(page).to have_content("Studio Location: #{studio2.location}")
-#       Story 1
-# Studio Index
-#
-# As a user,
-# When I visit the studio index page
-# I see a each studio's name and location
-# And underneath each studio, I see the titles of all of its movies.
+    end
 
+    it "displays the title of each of the studio's movies below the studio information" do
+      studio1 = Studio.create!(name: "Marvel Studios", location: "Hollywood, CA")
+      studio2 = Studio.create!(name: "Univeral Studios", location: "Santa Monica, CA")
+      movie1 = studio1.movies.create!(title: "Avengers", creation_year: 2011, genre: "SuperHero")
+      movie2 = studio1.movies.create!(title: "Infinity Wars", creation_year: 2017, genre: "Super Hero")
+      movie3 = studio2.movies.create!(title: "Jurassic Park", creation_year: 1993, genre: "Action/Adventure")
+
+      visit '/studios'
+
+      expect(page).to have_content("Popular Studio Films: #{movie1.title}")
+      expect(page).to have_content("Popular Studio Films: #{movie2.title}")
+      expect(page).to have_content("Popular Studio Films: #{movie3.title}")
+      #       Story 1
+      # Studio Index
+      #
+      # As a user,
+      # When I visit the studio index page
+      # I see a each studio's name and location
+      # And underneath each studio, I see the titles of all of its movies.
     end
   end
 end
