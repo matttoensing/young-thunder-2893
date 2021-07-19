f49c618ef815a4e9bf94dda8a97c1e71507fb760
Matt Toensing <matthew.toensing@gmail.com>
2021-07-19 10:17:55 -0700

    Update tests to include expectations with new actor

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -105,19 +105,8 @@ RSpec.describe 'movie show page' do
 
       expect(current_path).to eq("/movies/#{movie.id}")
       expect(page).to have_content("Scarlett Johansson")
+      expect("Scarlett Johansson").to appear_before(actor1.name)
       expect(page).to have_content("36")
-      #       Story 3
-      # Add an Actor to a Movie
-      #
-      # As a user,
-      # When I visit a movie show page,
-      # I do not see any actors listed that are not part of the movie
-      # And I see a form to add an actor to this movie
-      # When I fill in the form with the name of an actor that exists in the database
-      # And I click submit
-      # Then I am redirected back to that movie's show page
-      # And I see the actor's name is now listed
-      # (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
     end
   end
 end
